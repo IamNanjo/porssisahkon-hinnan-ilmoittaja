69aea347fc924ec670f36b68c5bd8719bd4f1c24
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-07-17 01:14:56 +0300

    Removed unused types

--- APITypes.ts
+++ APITypes.ts
@@ -1,7 +1,3 @@
-export type APIPriceResponse = {
-  price: number;
-};
-
 export type APILatestPricesResponse = {
   prices: {
     price: number;
@@ -9,7 +5,3 @@ export type APILatestPricesResponse = {
     endDate: string;
   }[];
 };
-
-const date = new Date();
-const today = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate()}`;
-const currentHour = `${date.getHours().toString().padStart(2, "0")}`;
