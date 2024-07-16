ec8db813ec2eec1cd4814f4a4535a4e8da917ef4
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-07-17 01:15:12 +0300

    Added script for multi platform building

---
+++ build-all.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+cd $(dirname $0)
+
+bun build index.ts --compile --target=bun-linux-x64-modern --outfile dist/hinta-tarkkailija-linux-x64
+bun build index.ts --compile --target=bun-linux-arm64 --outfile dist/hinta-tarkkailija-linux-arm64
+bun build index.ts --compile --target=bun-windows-x64-modern --outfile dist/hinta-tarkkailija-windows-x64
